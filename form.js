70e32bf042ccd8a1d9eedf10ea0810dd830f5f15
Oksanaka848 <okmitka@mail.ru>
2022-11-20 21:32:39 +0300

    ch

--- form.js
+++ form.js
@@ -12,6 +12,12 @@ let reg_first_name = document.getElementById ('reg_first_name');
 let reg_middle_name = document.getElementById ('reg_middle_name');
 let reg_email = document.getElementById ('reg_email');
 let birthday = document.getElementById ('birthday');
+button.addEventListener("click", function() {
+    checkAll();
+    ValidateEmail(reg_email);
+    changeColor();
+    checkDate();
+  })
 
 function checkAll(){
     let inputs=document.querySelectorAll("input");
@@ -38,14 +44,24 @@ if (birthday.value  == '') {
 document.getElementById ('error').innerHTML+= "Ваша дата рождения?<br>";
 } 
 
-else {
-alert(`Добро пожаловать, ${reg_first_name.value} !`);
-return false;}
+
 }
 
 }
+/*if (let = validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+    
+        
+       
+  }){
+    alert("Ваш адрес электронной почты введен неверно!");
+  }*/
 function ValidateEmail(reg_email) {
-    let mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+    let mailFormat = "[a-z0-9._%+-]+@[ a-z0-9.-]+.[a-z]{2,}$";
     if (reg_email.value.match(mailFormat)) {
         return true;
     }
@@ -55,16 +71,38 @@ function ValidateEmail(reg_email) {
     }
 }
 
+function checkDate(birthday) {
+        let pattern = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+let date= new Date();
+if (birthday == "" || birthday == null||!pattern.test(birthday)) {
+    alert("invalid date of birth should in yyyy-mm-dd");
+    return false;
+}
+else if(new Date(birthday).getTime() > date.getTime()){
+    alert("invalid date");
+    return false;
+}
+else{
+    alert("valid date");
+}
+}
+
 
 function changeColor(){
-    document.getElementsByClassName("switcher__control").classList.contains("switcher__control");
-    if ( document.getElementsByClassName("switcher__control").classList.contains("switcher__control") )
+    let i=0;
+    let cycl=document.getElementsByClassName("switcher__control")[i].classList.contains("switcher__control");
+    for (i=0; i< cycl.length; i++){
+
+     document.getElementsByClassName("switcher__control").classList.contains("switcher__control");
 
 document.getElementsByClassName("MyElement").classList.add('selected');
 }
-
+}
 /*function changeColor(){
     
     document.getElementsByClassName("switcher__control")[0].className += "selected";
 }
 */
+/*else {
+alert(`Добро пожаловать, ${reg_first_name.value} !`);
+}*/
