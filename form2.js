037c30fa3efd7ed9ef9bc9e0d84841869955bec6
Oksanaka848 <okmitka@mail.ru>
2022-11-20 22:01:06 +0300

    ch

--- form2.js
+++ form2.js
@@ -15,6 +15,7 @@ let birthday = document.getElementById ('birthday');
 button.addEventListener("click", function() {
     checkAll();
     ValidateEmail(reg_email);
+    changeColor();
     checkDate(birthday);
   })
 
@@ -42,9 +43,23 @@ if (reg_email.value == '') {
 if (birthday.value  == '') {
 document.getElementById ('error').innerHTML+= "Ваша дата рождения?<br>";
 } 
-}
+
+
 }
 
+}
+/*if (let = validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+    
+        
+       
+  }){
+    alert("Ваш адрес электронной почты введен неверно!");
+  }*/
 function ValidateEmail(reg_email) {
     let mailFormat = "[a-z0-9._%+-]+@[ a-z0-9.-]+.[a-z]{2,}$";
     if (reg_email.value.match(mailFormat)) {
@@ -56,6 +71,21 @@ function ValidateEmail(reg_email) {
     }
 }
 
+/*function checkDate() {
+        let pattern = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+let date= new Date();
+if (birthday == "" || birthday == null||!pattern.test(birthday)) {
+    alert("invalid date of birth should in yyyy-mm-dd");
+    return false;
+}
+else if(new Date(birthday).getTime() > date.getTime()){
+    alert("invalid date");
+    return false;
+}
+else{
+    alert("valid date");
+}
+}*/
 function checkDate(birthday) {
 var date_regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
 if (birthday.value.match(date_regex)) {
@@ -68,23 +98,21 @@ else {
 }
 }
 
-/*function changeColor(){
+function changeColor(){
     let i=0;
-    
+    let cycl=document.getElementsByClassName("switcher__control")[i].classList.contains("switcher__control");
     for (i=0; i< cycl.length; i++){
 
      document.getElementsByClassName("switcher__control").classList.contains("switcher__control");
 
 document.getElementsByClassName("MyElement").classList.add('selected');
 }
-}*/
-/*function changeColor(){
-    let cycl=document.getElementsByName("gender").classList.contains("switcher__control");
-    for (i=0; i< cycl.length; i++){
-        document.getElementsByName("gender").className += "selected";
 }
-}*/
-
+/*function changeColor(){
+    
+    document.getElementsByClassName("switcher__control")[0].className += "selected";
+}
+*/
 /*else {
 alert(`Добро пожаловать, ${reg_first_name.value} !`);
 }*/
